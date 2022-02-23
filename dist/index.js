40b622e3b4288a856be7c2240ff9e54303afbaa1
unknown <gtrachev9@gmail.com>
2022-02-23 22:33:43 +0200

    fixing errors

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("ap")
+console.log("ap");
+import express from "express";
+const app = express();
+app.all("*", (req, res, next) => {
+  next(new AppError("Page not found.", 404));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`LISTENING ON PORT: ${PORT} `);
+});
