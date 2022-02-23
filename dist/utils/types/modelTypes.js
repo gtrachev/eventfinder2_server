0d7671bdf85e50bddfb11d288837a034d4c71fed
unknown <gtrachev9@gmail.com>
2022-02-23 23:15:35 +0200

    fixing errors

--- dist/utils/types/modelTypes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AgeGroupEnum = void 0;
-var AgeGroupEnum;
-(function (AgeGroupEnum) {
-    AgeGroupEnum["all"] = "all";
-    AgeGroupEnum["over"] = "over";
-})(AgeGroupEnum = exports.AgeGroupEnum || (exports.AgeGroupEnum = {}));
