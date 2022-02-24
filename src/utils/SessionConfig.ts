f9c5a73724a612c1ae02cdc8da8d7e68caa105be
unknown <gtrachev9@gmail.com>
2022-02-24 20:50:23 +0200

    fixing errors

--- src/utils/SessionConfig.ts
+++ src/utils/SessionConfig.ts
@@ -26,6 +26,8 @@ const sessionConfig: SessionOptions = {
   saveUninitialized: true,
   cookie: {
     // secure: true,
+    httpOnly: false,
+    sameSite: "none",
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
